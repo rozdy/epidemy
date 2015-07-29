64134f5b19e0e45212cf6e75ab41e4311d9c9ea7
s-kim <sergejjkim@gmail.com>
2015-07-29 23:25:36 +0300

    feature/steps InvalidCellException.java refactoring

--- app/src/main/java/sorry/no/domain/test_project/InvalidCellException.java
+++ app/src/main/java/sorry/no/domain/test_project/InvalidCellException.java
@@ -4,12 +4,8 @@ package sorry.no.domain.test_project;
  * Created by hex on 7/22/2015.
  */
 public class InvalidCellException extends Exception {
-    private Cell cell;
-    private String descripton;
 
     public InvalidCellException(Cell cell, String description) {
-        super();
-        this.cell = cell;
-        this.descripton = description;
+        super(description + "\nOn cell: " + cell);
     }
 }
