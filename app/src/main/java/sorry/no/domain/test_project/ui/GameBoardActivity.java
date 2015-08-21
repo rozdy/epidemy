fe75815d5c2fbf69956780d7f69745e2e2bb94bd
rozdy <rozdy@mail.ru>
2015-08-21 13:20:43 +0300

    Code cleanup.

--- app/src/main/java/sorry/no/domain/test_project/ui/GameBoardActivity.java
+++ app/src/main/java/sorry/no/domain/test_project/ui/GameBoardActivity.java
@@ -7,8 +7,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -30,50 +29,51 @@ public class GameBoardActivity extends ActionBarActivity {
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setNumColumns(Game.getInstance().getBoard().getWidth() + 1);
         gridview.setAdapter(new BoardImageAdapter(this));
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                BoardImageAdapter adapter = (BoardImageAdapter) parent.getAdapter();
-                try {
-                    Toast toast;
-                    switch (Game.getInstance().makeAMove(Game.getInstance().getActivePlayer(),
-                            adapter.getXByPosition(position), adapter.getYByPosition(position))) {
-                        case Board.MARK_PLACED:
-                        case Board.WALL_PLACED:
-                            adapter.notifyDataSetChanged();
-                            view.invalidate();
-                            break;
-                        case Game.GAME_FINISH_NO_MOVES:
-                            Game.finish(Game.GAME_FINISH_NO_MOVES);
-                            showFinalStats();
-                            break;
-                        case Game.GAME_FINISH_NO_MARKS:
-                            Game.finish(Game.GAME_FINISH_NO_MARKS);
-                            showFinalStats();
-                            break;
-                        case Board.UNREACHABLE_CELL:
-                            toast = Toast.makeText(parent.getContext(), R.string.UNREACHABLE_CELL, Toast.LENGTH_SHORT);
-                            toast.show();
-                            break;
-                        case Board.ENEMY_WALL_HIT:
-                            toast = Toast.makeText(parent.getContext(), R.string.ENEMY_WALL_HIT, Toast.LENGTH_SHORT);
-                            toast.show();
-                            break;
-                        case Board.OWN_CROSS_HIT:
-                            toast = Toast.makeText(parent.getContext(), R.string.OWN_CROSS_HIT, Toast.LENGTH_SHORT);
-                            toast.show();
-                            break;
-                        case Board.OWN_WALL_HIT:
-                            toast = Toast.makeText(parent.getContext(), R.string.OWN_WALL_HIT, Toast.LENGTH_SHORT);
-                            toast.show();
-                            break;
-                    }
-                } catch (InvalidPositionException | InvalidCellException | InvalidMoveException e) {
-                    Toast toast = Toast.makeText(parent.getContext(), e.toString(), Toast.LENGTH_LONG);
-                    toast.show();
+        gridview.setOnItemClickListener(getOnItemClickListener());
+    }
+
+    public OnItemClickListener getOnItemClickListener() {
+        return (parent, view, position, id) -> {
+            BoardImageAdapter adapter = (BoardImageAdapter) parent.getAdapter();
+            try {
+                Toast toast;
+                switch (Game.getInstance().makeAMove(Game.getInstance().getActivePlayer(),
+                        adapter.getXByPosition(position), adapter.getYByPosition(position))) {
+                    case Board.MARK_PLACED:
+                    case Board.WALL_PLACED:
+                        adapter.notifyDataSetChanged();
+                        view.invalidate();
+                        break;
+                    case Game.GAME_FINISH_NO_MOVES:
+                        Game.finish(Game.GAME_FINISH_NO_MOVES);
+                        showFinalStats();
+                        break;
+                    case Game.GAME_FINISH_NO_MARKS:
+                        Game.finish(Game.GAME_FINISH_NO_MARKS);
+                        showFinalStats();
+                        break;
+                    case Board.UNREACHABLE_CELL:
+                        toast = Toast.makeText(parent.getContext(), R.string.UNREACHABLE_CELL, Toast.LENGTH_SHORT);
+                        toast.show();
+                        break;
+                    case Board.ENEMY_WALL_HIT:
+                        toast = Toast.makeText(parent.getContext(), R.string.ENEMY_WALL_HIT, Toast.LENGTH_SHORT);
+                        toast.show();
+                        break;
+                    case Board.OWN_CROSS_HIT:
+                        toast = Toast.makeText(parent.getContext(), R.string.OWN_CROSS_HIT, Toast.LENGTH_SHORT);
+                        toast.show();
+                        break;
+                    case Board.OWN_WALL_HIT:
+                        toast = Toast.makeText(parent.getContext(), R.string.OWN_WALL_HIT, Toast.LENGTH_SHORT);
+                        toast.show();
+                        break;
                 }
+            } catch (InvalidPositionException | InvalidCellException | InvalidMoveException e) {
+                Toast toast = Toast.makeText(parent.getContext(), e.toString(), Toast.LENGTH_LONG);
+                toast.show();
             }
-        });
+        };
     }
 
     @Override
