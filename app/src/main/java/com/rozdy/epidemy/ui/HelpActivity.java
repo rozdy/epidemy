2a9529fbc9ad6ccc9f6bd725474f21d84ec602b8
s-kim <sergejjkim@gmail.com>
2015-12-13 18:11:27 +0200

    features/help add HelpActivity.java

---
+++ app/src/main/java/com/rozdy/epidemy/ui/HelpActivity.java
@@ -0,0 +1,17 @@
+package com.rozdy.epidemy.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.rozdy.epidemy.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
